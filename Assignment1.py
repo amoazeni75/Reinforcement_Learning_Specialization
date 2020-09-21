98fd7332611173f80f435c55d1a604ee7e0fc6f5
S.Alireza Moazeni <amoazeni75@gmail.com>
2020-09-21 08:27:59 +0330

    Test for argmax function added

--- Assignment1.py
+++ Assignment1.py
@@ -1,3 +1,15 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
+
+from rlglue.rl_glue import RLGlue
+import main_agent
+import ten_arm_env
+import test_env
+
+
 # -----------
 # Graded Cell
 # -----------
@@ -7,20 +19,59 @@ def argmax(q_values):
     with the highest value. Breaks ties randomly.
     returns: int - the index of the highest value in q_values
     """
+    # if a value in q_values is greater than the highest value update top and reset ties to zero
+    # if a value is equal to top value add the index to ties
+    # return a random selection from ties.
     top_value = float("-inf")
     ties = []
 
     for i in range(len(q_values)):
         if q_values[i] > top_value:
             top_value = q_values[i]
-            ties = []
-            ties.append(i)
+            ties = [i]
         elif q_values[i] == top_value:
             ties.append(i)
-
-        # if a value in q_values is greater than the highest value update top and reset ties to zero
-        # if a value is equal to top value add the index to ties
-        # return a random selection from ties.
-        # YOUR CODE HERE
-        raise NotImplementedError()
     return np.random.choice(ties)
+
+
+# --------------
+# Debugging Cell
+# --------------
+# Feel free to make any changes to this cell to debug your code
+
+test_array = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+assert argmax(test_array) == 8, "Check your argmax implementation returns the index of the largest value"
+
+# make sure np.random.choice is called correctly
+np.random.seed(0)
+test_array = [1, 0, 0, 1]
+
+assert argmax(test_array) == 0
+
+# -----------
+# Tested Cell
+# -----------
+# The contents of the cell will be tested by the autograder.
+# If they do not pass here, they will not pass there.
+
+test_array = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+assert argmax(test_array) == 8, "Check your argmax implementation returns the index of the largest value"
+
+# set random seed so results are deterministic
+np.random.seed(0)
+test_array = [1, 0, 0, 1]
+
+counts = [0, 0, 0, 0]
+for _ in range(100):
+    a = argmax(test_array)
+    counts[a] += 1
+
+# make sure argmax does not always choose first entry
+assert counts[0] != 100, "Make sure your argmax implementation randomly choooses among the largest values."
+
+# make sure argmax does not always choose last entry
+assert counts[3] != 100, "Make sure your argmax implementation randomly choooses among the largest values."
+
+# make sure the random number generator is called exactly once whenver `argmax` is called
+expected = [44, 0, 0, 56]  # <-- notice not perfectly uniform due to randomness
+assert counts == expected
